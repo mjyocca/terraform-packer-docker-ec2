d3061575326c6aa5f6932db4bdc5b774d33aeb19
Michael Yocca <mjyocca@gmail.com>
2022-11-13 08:06:11 -0800

    cleanup packer ami setup script

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -7,10 +7,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable'
 sudo apt-get update
-# 
 sudo apt-get install -y amazon-ecr-credential-helper
-# 
 sudo apt-get install -y docker-ce
-
 # sudo groupadd docker
 sudo usermod -aG docker ubuntu
