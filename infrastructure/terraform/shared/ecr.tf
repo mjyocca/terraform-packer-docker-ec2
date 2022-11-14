fdfa2c94ab26cbb8942e8e29386f96bd9ca1d2b7
Michael Yocca <mjyocca@gmail.com>
2022-11-13 20:31:21 -0800

    add force_delete argument to ecr resource

--- infrastructure/terraform/shared/ecr.tf
+++ infrastructure/terraform/shared/ecr.tf
@@ -13,7 +13,7 @@ provider "aws" {
 resource "aws_ecr_repository" "ecr" {
   name                 = "terraform-packer-docker-project"
   image_tag_mutability = "MUTABLE"
-
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
