269c1b98c4d833669c1866e4858fdd9a5a5a088c
Michael Yocca <mjyocca@gmail.com>
2022-11-26 12:26:55 -0800

    add application version in response handler

--- app/server.js
+++ app/server.js
@@ -6,13 +6,14 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const VERSION = process.env.VERSION;
+const APP_VERSION = "1.0.0";
 const DEPLOYMENT = process.env.DEPLOYMENT;
 const COUNT = process.env.COUNT;
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`Version ${VERSION} - (${DEPLOYMENT}) #${COUNT}!, \n`);
+  res.send(`Version ${VERSION} - (${DEPLOYMENT}) #${COUNT}!, App Version: ${APP_VERSION}, \n`);
 });
 
 app.listen(PORT, () => {
